09c52e3162969bdc1392edd60ae0b1b0b6de7c97
Vitor Tavares <vitor.tavares.leite@gmail.com>
2024-04-08 14:57:54 -0300

    Update and rename LostMov.cs to LostMidia.cs

--- LostMidia.cs
+++ LostMidia.cs
@@ -23,3 +23,30 @@
             transform.Translate(-1*speed * Time.deltaTime, 0, 0);
         }
     }
+
+
+
+   
+   using Unity3D;
+   
+    //steam vr não me lembro corretamente 
+    public float speed = 10;
+
+
+
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        if (RUN.GetStateDown(SteamVR_Input_Sources.Any)){ 
+
+            RUN2 == true;
+        } 
+         if(RUN2 == true) {
+            speed = 15; 
+        }
+        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new  Vector3)(cinput.axis.x, 0, cinput.axis.Transform.Position += speed * Time.deltatime * Vector3.ProjectOnPlane(direction, Vector3.up))
+    }
